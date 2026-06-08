7e8457bbf403711b2da439899c57040250985e4f
IsaacBighi <isaacbighi.dev@gmail.com>
2026-06-08 18:31:46 -0300

    refactor

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import { useEffect } from 'react';
-import { ThemeProvider } from 'styled-components/native';
+import { ThemeProvider } from 'styled-components';
 import { Routes } from './src/routes';
 import { initDatabase } from './src/sqlite';
 import { theme } from './src/styles/theme';
--- src/components/ProductCard/styles.ts
+++ src/components/ProductCard/styles.ts
@@ -1,47 +1,33 @@
-import styled from 'styled-components/native';
+import { Text, View } from 'react-native';
+import styled from 'styled-components';
 
-export const Container = styled.View`
+export const Container = styled(View)`
   width: 95%;
-
   padding: 16px;
-
   margin: 0 auto 16px;
-
   border-radius: 16px;
-
   background-color: #fff;
-
   elevation: 4;
-
   shadow-color: #000;
-
-  shadow-offset: 3px 5px;
-
   shadow-opacity: 0.15;
-
   shadow-radius: 5px;
 `;
 
-export const Name = styled.Text`
+export const Name = styled(Text)`
   font-size: 18px;
   font-weight: bold;
-
   color: ${({ theme }) => theme.colors.text};
 `;
 
-export const Category = styled.Text`
+export const Category = styled(Text)`
   font-size: 14px;
-
   margin-top: 4px;
-
   color: #666;
 `;
 
-export const Price = styled.Text`
+export const Price = styled(Text)`
   font-size: 16px;
   font-weight: bold;
-
   margin-top: 8px;
-
   color: ${({ theme }) => theme.colors.primary};
 `;
